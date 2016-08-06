6b31b0f7ed081f6970a0fb54cd4dc6f7981a0c77
James Whitaker <jameswhitakerwork@gmail.com>
2016-08-06 22:44:11 +0800

    added animation

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.listview import ListItemButton
-from kivy.properties import ObjectProperty, ListProperty
+from kivy.properties import ObjectProperty, ListProperty, NumericProperty, StringProperty
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.storage.jsonstore import JsonStore
+from kivy.uix.slider import Slider
 from kivy.uix.popup import Popup
+from kivy.uix.bubble import Bubble
+from kivy.animation import Animation, AnimationTransition
 from functools import partial
 from kivy.clock import Clock
 
@@ -64,7 +67,7 @@ class ReceivingRoot(BoxLayout):
 	def popup(self, title, message):
 		# create content and add to the popup
 		content = Button(text=message)
-		popup = Popup(content=content, title=title, auto_dismiss=False)
+		popup = Popup(content=content, title=title, auto_dismiss=False, size_hint=(0.5, 0.5))
 
 		# bind the on_press event of the button to the dismiss function
 		content.bind(on_press=popup.dismiss)
@@ -103,7 +106,7 @@ class Item_List(GridLayout):
 				or
 				search in key
 				):
-				selector.label.text =  key + ':   ' + store['itemslist']['items'][key]
+				selector.label.text =  key + ':   ' + store['itemslist']['items'][key][0] + "  " + store['itemslist']['items'][key][1] + ")"
 				selector.id = key
 				self.add_widget(selector)
 
@@ -113,33 +116,42 @@ class Item_List(GridLayout):
 		self.clear_widgets()
 		for key in store['itemslist']['items']:
 			selector = (Item_Selector())
-			selector.label.text = key + ':   ' + store['itemslist']['items'][key]
+			selector.label.text = key + ':   ' + store['itemslist']['items'][key][0] + " (" + store['itemslist']['items'][key][1] + ")"
 			selector.id = key
 			self.add_widget(selector)
 
 
 	def add_item(self, itemid, amount):
-		global store
-		global currentreport
-		itemid = int(itemid)
-		entry = [itemid, int(amount)]
-		print 'adding item code %i, amount: %s' % (itemid, amount)
-		item_exists = 0
-		for i in range(0, len(currentreport)):
-			if entry[0] == currentreport[i][0]:
-				print entry[0]
-				print currentreport[i][0]
-				print 'Item already exists in current report'
-				print 'prev amount: ' + str(currentreport[i][1])
-				currentreport[i][1] += int(amount)
-				print 'new amount: ' + str(currentreport[i][1])
-				item_exists = 1
-		if item_exists == 0:
-				print 'New item added to current report'
-				currentreport.append(entry)
+		if int(amount) > 0:
+			global store
+			global currentreport
+			itemid = int(itemid)
+			entry = [itemid, int(amount)]
+			print 'adding item code %i, amount: %s' % (itemid, amount)
+			item_exists = 0
+			for i in range(0, len(currentreport)):
+				if entry[0] == currentreport[i][0]:
+					print entry[0]
+					print currentreport[i][0]
+					print 'Item already exists in current report'
+					print 'prev amount: ' + str(currentreport[i][1])
+					currentreport[i][1] += int(amount)
+					print 'new amount: ' + str(currentreport[i][1])
+					item_exists = 1
+			if item_exists == 0:
+					print 'New item added to current report'
+					currentreport.append(entry)
 
 
-		print currentreport
+
+			print currentreport
+		else:
+			pass
+
+
+
+class Item_Slider(Slider):
+	secretvalue = NumericProperty()
 
 
 
@@ -166,7 +178,8 @@ class Current_List(GridLayout):
 		for entry in currentreport:
 
 			newselector = Current_Selector()
-			newselector.label.text = '%s: %i' % (lookup(entry[0]), entry[1])
+			name, uom = lookup(entry[0])
+			newselector.label.text = '%s:    %i (%s)' % (name, entry[1], uom)
 			newselector.code = entry[0]
 			self.add_widget(newselector)
 
@@ -175,7 +188,14 @@ class Current_List(GridLayout):
 
 
 class Current_Selector(BoxLayout):
-	pass
+
+	def __init__(self, **kwargs):
+		super(Current_Selector, self).__init__(**kwargs)
+		self.code = StringProperty()
+
+	def remove_item_on_delete(self):
+		print self.code
+
 
 class Tabs(TabbedPanel):
 	def __init__(self, **kwargs):
@@ -191,6 +211,21 @@ class Item_Selector(BoxLayout):
 
 class Search_Bar(BoxLayout):
 	search_input = ObjectProperty()
+
+
+class Add_Button(Button):
+	def change_colour(self, widgetid):
+		self.anim = Animation(background_color=(0,1,0,1), size_hint_x = 0.2, t='out_circ', duration=0.2) + Animation(duration=0.5) + Animation(background_color=(0,1,0,0.5), size_hint_x = 0.1, t='out_circ')
+		self.anim.start(self)
+		self.text = 'Added!'
+		print 'animation fired'
+
+
+
+
+
+
+
 
 
 def authgoogle():
@@ -227,7 +262,7 @@ def downloadrows(wks):
 	masterlist = {}
 
 	for i in range(0, len(item_list)):
-		masterlist[code_list[i]] = item_list[i]
+		masterlist[code_list[i]] = [item_list[i], uom_list[i]]
 #	print masterlist
 	print 'items joined'
 
@@ -245,10 +280,23 @@ def make_lookup_table():
 
 def lookup(key):
 	for entry in lookup_table:
-		if key in entry:
-			print entry[1]
-			return entry[1]
+		if key == entry[0]:
+			return entry[1][0], entry[1][1]
 
+def limit_to_100(value):
+	try:
+		value = int(value)
+		if value >= 100:
+			value = 100
+		return value
+	except:
+		return 0
+
+def maybe_clear(widget, string):
+	if int(string) == 0:
+		widget.text = ""
+	else:
+		return string
 
 if __name__ == "__main__":
 	lookup_table = make_lookup_table()
